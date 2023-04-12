d9ef2ccd0d58874bbb1130cc93df5ee3ef64b7a7
Aleksei Kornev <aleksei.kornev@gmail.com>
2023-04-12 21:14:19 +0400

    Turn off popup with webinar 1204

--- js/main.js
+++ js/main.js
@@ -27,10 +27,10 @@
 	var popup = document.getElementById("popup");
 	if (popup != null) {
 		var closeBtn = document.getElementsByClassName("closepopup")[0];
-
-		setTimeout(function () {
-			popup.style.display = "block";
-		}, 15000);
+// Turn off untill next activity
+// 		setTimeout(function () {
+// 			popup.style.display = "block";
+// 		}, 15000);
 
 		// When the user clicks on <span> (x), close the modal
 		closeBtn.onclick = function () {
